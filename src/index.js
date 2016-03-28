ad63152a8cab0e209b311884926a622f264e79be
Nicolas Escalante <nlante@gmail.com>
2016-03-27 22:55:09 -0300

    clean code, refactor api

--- src/index.js
+++ src/index.js
@@ -1,155 +1,73 @@
 'use strict';
 
 var fs = require('fs');
+var isDescendant = require('is-descendant');
 
 angular.module('multiple-select', ['ng'])
 .directive('multipleSelect', [
-  '$parse', '$document',
-  function ($parse, $document) {
+  '$document',
+  function ($document) {
     return {
       restrict: 'AE',
       require: '?ngModel',
       scope: {
-        sourceList: '=',
+        items: '=',
+        ngModel: '=',
+        isDisabled: '=?',
+        searchLabel: '@?',
+        selectAllLabel: '@?',
+        selectNoneLabel: '@?',
+        showSearch: '=?',
+        showSelectors: '=?',
+        buttonLabel: '&?',
         itemLabel: '&',
-        buttonLabel: '&',
-        isDisabled: '=',
-        onItemClick: '&',
-        onSelectAll: '&',
-        onSelectNone: '&',
-        onOpen: '&',
-        onClose: '&',
-        modelDescription: '=',
-        selectAllLabel: '=',
-        selectNoneLabel: '=',
-        searchLabel: '='
       },
       template: fs.readFileSync(__dirname + '/template.html', 'utf8'),
-      link: function (scope, element, attrs, ngModel) {
-        scope.isVisible = false;
-        scope.helpers = {
-          all: true,
-          none: true,
-          filter: true,
-        };
-
-        scope.labels = {
-          selectAll: attrs.selectAllLabel || 'Select all',
-          selectNone: attrs.selectNoneLabel || 'Select none',
-          search: attrs.searchLabel || 'Search'
-        };
-
-        scope.list = [];
-
+      link: function (scope, element) {
         $document[0].addEventListener('click', function (event) {
-          var target = event.target;
-
-          if (!isDescendant(element[0], target)) {
+          if (!isDescendant(element[0], event.target)) {
             scope.$apply(function () {
               scope.isVisible = false;
-              scope.onClose();
             });
           }
         });
-
-        scope.$watch(function () {
-          return scope.sourceList;
-        }, function (source) {
-          if (scope.sourceList) {
-            scope.list = scope.sourceList.map(function (item) {
-              return {
-                label: scope.itemLabel(item),
-                isSelected: false,
-                getSourceItem: function () {
-                  return item;
-                }
-              };
-            });
-          }
-          else {
-            scope.list = [];
-          }
-        });
-
-        scope.toggle = toggle;
-        scope.updateCheck = updateCheck;
-        scope.selectAll = selectAll;
-        scope.selectNone = selectNone;
-
-        if (!attrs.buttonLabel) {
-          scope.buttonLabel = function (items) {
-            return items && items.length ? items.length + ' items' : attrs.modelDescription;
-          };
-        }
-
-        refreshLabel();
-
-        scope.$watch(function () {
-          return ngModel.$modelValue;
-        }, updateList, true);
-
-        function toggle() {
-          scope.isVisible = !scope.isVisible;
-
-          if (scope.isVisible) {
-            scope.onOpen();
-          } else {
-            scope.onClose();
-          }
-        }
-
-        function updateCheck(item) {
-          var model = scope.list
-            .filter(function (item) {
-              return item.isSelected;
-            })
-            .map(function (item) {
-              return item.getSourceItem();
-            });
-
-          ngModel.$setViewValue(model);
-          scope.onItemClick({ item: item.getSourceItem() });
-        }
-
-        function selectAll() {
-          ngModel.$setViewValue(scope.sourceList.slice());
-          scope.onSelectAll();
-        }
-
-        function selectNone() {
-          ngModel.$setViewValue([]);
-          scope.onSelectNone();
-        }
-
-        function refreshLabel() {
-          scope.parsedLabel = $parse(scope.buttonLabel)(ngModel.$modelValue);
-        }
-
-        function updateList(obj) {
-          scope.list.forEach(function (item) {
-            var isSelected = (obj || []).some(function (modelItem) {
-              return item.getSourceItem() === modelItem;
-            });
-
-            item.isSelected = isSelected;
-          });
-
-          refreshLabel();
-        }
-
-        function isDescendant(parent, child) {
-          var node = child.parentNode;
-
-          while (node !== null) {
-            if (node === parent) {
-              return true;
-            }
-
-            node = node.parentNode;
-          }
-
-          return false;
-        }
-      }
+      },
+      controller: 'multipleSelectController'
     };
-  }]);
+  }
+])
+.controller('multipleSelectController', [
+  '$scope',
+  function multipleSelectController($scope) {
+    $scope.selectAll = selectAll;
+    $scope.selectNone = selectNone;
+    $scope.toggleSelection = toggleSelection;
+    $scope.cleanSearch = cleanSearch;
+
+    function selectAll() {
+      $scope.ngModel = $scope.items.slice();
+    }
+
+    function selectNone() {
+      $scope.ngModel = [];
+    }
+
+    function toggleSelection (item) {
+      var itemIndex;
+
+      $scope.ngModel = $scope.ngModel || [];
+      itemIndex = $scope.ngModel.indexOf(item);
+
+      if (itemIndex > -1) {
+        $scope.ngModel.splice(itemIndex, 1);
+      }
+      else {
+        $scope.ngModel.push(item);
+      }
+    }
+
+    function cleanSearch() {
+      $scope.search = '';
+    }
+  }
+]);
